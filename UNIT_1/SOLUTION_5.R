5485125d64c24d11335c2a382bc1f96256d8c671
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 19:39:47 +0530

    Create SOLUTION_5.R

---
+++ UNIT_1/SOLUTION_5.R
@@ -0,0 +1,11 @@
+xi<-6:12
+xi
+xii<-rep(5.3,times=3)
+xii
+xiii<--3
+xiii
+xiv <- seq(from = 102 , to = length(repeated_vec) , length.out = 9)
+xiv
+sol4<-c(xi,xii,xiii,xiv)
+sol4
+print(length(sol4) == 20)
