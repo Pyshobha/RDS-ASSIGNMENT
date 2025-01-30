d996b61a113ddf5ba6d33b653d81abad4dd45fb4
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:31:25 +0530

    Update and rename sol10.R to SOLUTION_10.R

---
+++ UNIT_1/SOLUTION_10.R
@@ -0,0 +1,7 @@
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+my_vector
+my_vector <- sort(my_vector)
+my_vector
--- UNIT_1/sol10.R
+++
@@ -1,6 +0,0 @@
-sol8<-sol6[ -c(1,length(sol6))]
-sol8
-sol6<-c(z,sol8)
-sol6
-sol6<-sort(sol6)
-sol6
