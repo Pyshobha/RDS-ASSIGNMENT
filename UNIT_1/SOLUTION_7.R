a1746415f3d3a69419474acd91d873a62a81f49a
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:24:33 +0530

    Update SOLUTION_7.R

--- UNIT_1/SOLUTION_7.R
+++ UNIT_1/SOLUTION_7.R
@@ -1,4 +1,6 @@
-x<-sol6[1]
-y<-sol6[length(sol6)]
-z<-c(x,y)
-z
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+first_last <- c(my_vector[1], my_vector[length(my_vector)])
+first_last
