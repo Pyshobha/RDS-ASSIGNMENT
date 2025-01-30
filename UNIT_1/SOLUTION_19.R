21899c7c906d2467f7ed7ff8ca62a50d7b2e0c50
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 21:04:30 +0530

    Create SOLUTION_19.R

---
+++ UNIT_1/SOLUTION_19.R
@@ -0,0 +1,9 @@
+original_vector <- c(-2, 4, -6, 8, -10)
+repeated_vector <- rep(original_vector, times = 3)
+each_repeated_vector <- rep(original_vector, each = 5)
+sorted_repeated_vector <- sort(repeated_vector)
+sorted_each_repeated_vector <- sort(each_repeated_vector)
+repeated_vector
+each_repeated_vector
+sorted_repeated_vector
+sorted_each_repeated_vector
