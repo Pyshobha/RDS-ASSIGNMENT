98012c4db8686da9a2f827f94c784d5da5164dec
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:21:41 +0530

    Update SOLUTION_6.R

--- UNIT_1/SOLUTION_6.R
+++ UNIT_1/SOLUTION_6.R
@@ -1,11 +1,5 @@
-i<-seq(from=3,to=6,length.out=5)
-i
-
-ii<-rep(c(2,-5.1,-33),times=2)
-ii
-
-iii<-7/42+2
-iii
-
-sol6<-c(i,ii,iii)
-sol6
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+my_vector
