11f94df2d87fc6a545d3ae6a9293a6c45988798d
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:28:55 +0530

    Update SOLUTION_9.R

--- UNIT_1/SOLUTION_9.R
+++ UNIT_1/SOLUTION_9.R
@@ -1,4 +1,9 @@
-sol8<-sol6[ -c(1,length(sol6))]
-sol8
-sol6<-c(z,sol8)
-sol6
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+my_vector
+first_last <- c(my_vector[1], my_vector[length(my_vector)])
+omit_first_last <- my_vector[-c(1, length(my_vector))]
+reconstructed_vector <- c(first_last[1], omit_first_last, first_last[2])
+reconstructed_vector
