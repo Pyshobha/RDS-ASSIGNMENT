a2741e43afb727377a39a6b82ccaf62186c43066
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:54:57 +0530

    Update and rename SOLUTION_11.R to SOLUTION_16.R

--- UNIT_1/SOLUTION_11.R
+++
@@ -1,3 +0,0 @@
-rev <- 10:1
-sorted <- sort(1:10, decreasing = TRUE)
-identical(rev, sorted)
---
+++ UNIT_1/SOLUTION_16.R
@@ -0,0 +1,5 @@
+vector1 <- c(2, 4, 6)
+vector2 <- c(1, 2)
+
+result_vector <- rep(vector1, vector2)
+result_vector
