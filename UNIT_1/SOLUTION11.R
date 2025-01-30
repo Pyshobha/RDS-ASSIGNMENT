a8876d39d6d02434dac1720baa0f420ebcd04d49
Shobha Jangade <jangadeshobha@gmail.com>
2025-01-30 20:35:04 +0530

    Update and rename sol11.R to SOLUTION11.R

---
+++ UNIT_1/SOLUTION11.R
@@ -0,0 +1,9 @@
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+my_vector
+my_vector <- sort(my_vector)
+reversed_vector <- my_vector[length(my_vector):1]
+sorted_decreasing <- sort(my_vector, decreasing = TRUE)
+identical(reversed_vector, sorted_decreasing)
