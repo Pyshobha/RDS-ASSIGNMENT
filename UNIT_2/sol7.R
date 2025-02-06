75e90a5926cf82634e4535ade2030900b32090aa
Shobha Jangade <jangadeshobha@gmail.com>
2025-02-06 11:46:24 +0530

    Create sol7.R

---
+++ UNIT_2/sol7.R
@@ -0,0 +1,5 @@
+# Question 7: Compute given expression
+matrix_3 <- matrix(c(1,2,7,2,4,6), nrow=3)
+matrix_4 <- matrix(c(10,20,30,40,50,60), nrow=3)
+result_7 <- (2/7) * (matrix_3 - matrix_4)
+result_7
