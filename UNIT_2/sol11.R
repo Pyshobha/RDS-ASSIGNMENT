a57164a26ad4e4945cd50de9ffa3721abf39e2db
Shobha Jangade <jangadeshobha@gmail.com>
2025-02-06 11:50:14 +0530

    Create sol11.R

---
+++ UNIT_2/sol11.R
@@ -0,0 +1,4 @@
+# 11. Extract and store as a new object the fourth- and first-row elements, in that order, 
+# of the second column only of all layers of (10.)
+new_obj <-array_10[c(4, 1), 2, ]
+new_obj
