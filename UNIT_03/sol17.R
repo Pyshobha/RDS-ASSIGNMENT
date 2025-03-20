6149d82dde9ccad0164c670b42d2e5c3f0c1358a
Shobha Jangade <jangadeshobha@gmail.com>
2025-03-20 13:24:14 +0530

    Create sol17.R

---
+++ UNIT_03/sol17.R
@@ -0,0 +1,7 @@
+# 17. Logical operations on two vectors
+
+log_vec1 <- c(TRUE, FALSE, TRUE, FALSE)
+log_vec2 <- c(FALSE, TRUE, TRUE, FALSE)
+print(log_vec1 & log_vec2)  
+print(log_vec1 | log_vec2)  
+print(!log_vec1) 
