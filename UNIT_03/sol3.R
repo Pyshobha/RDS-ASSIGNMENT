f79f20b895df11a94af78b95956a6c4c17673a08
Shobha Jangade <jangadeshobha@gmail.com>
2025-03-20 13:15:00 +0530

    Update sol3.R

--- UNIT_03/sol3.R
+++ UNIT_03/sol3.R
@@ -1,6 +1,7 @@
-##3. Finding Indices Where Two Vectors Differ
+# 3. Check element-wise equality and output differing indices
 
 vec1 <- c(1, 2, 3, 4, 5)
-vec2 <- c(1, 2, 0, 4, 6)
-different_indices <- which(vec1 != vec2)
+vec2 <- c(1, 3, 3, 2, 5)
+equal_check <- vec1 == vec2
+different_indices <- which(!equal_check)
 print(different_indices)
