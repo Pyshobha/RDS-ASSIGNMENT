94f63c2b4965e91ceb9cf095066939abde951e05
Shobha Jangade <jangadeshobha@gmail.com>
2025-03-20 13:15:51 +0530

    Update sol4.R

--- UNIT_03/sol4.R
+++ UNIT_03/sol4.R
@@ -1,8 +1,6 @@
-##4. Creating and Comparing 3×4 Matrices
+# 4. Create a 3x4 logical matrix and compare with a numeric matrix
 
-logical_matrix <- matrix(c(TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, FALSE), nrow=3, ncol=4)
+logical_matrix <- matrix(rep(c(TRUE, FALSE, TRUE, FALSE), length.out = 12), nrow = 3, ncol = 4)
+numeric_matrix <- matrix(1:12, nrow = 3, ncol = 4)
 print(logical_matrix)
-
-numeric_matrix <- matrix(1:12, nrow=3, ncol=4)
-comparison <- logical_matrix == numeric_matrix
-print(comparison)
+print(numeric_matrix == logical_matrix)
