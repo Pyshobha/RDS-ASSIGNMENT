df433a132a498d6f5a341db05990473369a99e51
Shobha Jangade <jangadeshobha@gmail.com>
2025-04-30 10:17:15 +0530

    Create sol11.R

---
+++ UNIT_04/sol11.R
@@ -0,0 +1,3 @@
+# 11. Create a nested list where one element is itself a list containing a numeric vector and a character vector:
+
+nested_list <- list(inner_list = list(numbers = c(1, 2, 3), letters = c("a", "b", "c")))
