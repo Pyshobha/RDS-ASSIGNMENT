5c112ce08381571317fad6fbf3b5fcd6e946e92a
Shobha Jangade <jangadeshobha@gmail.com>
2025-04-30 10:21:22 +0530

    Create sol16.R

---
+++ UNIT_04/sol16.R
@@ -0,0 +1,6 @@
+# 16. Demonstrate the difference between single bracket [ ] slicing and double bracket [[ ]] referencing:
+
+example_list <- list(a = 10, b = 20)
+
+example_list[1]    
+example_list[[1]]  
